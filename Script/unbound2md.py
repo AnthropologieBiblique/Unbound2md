e9bf53eca65fb906e6dd9e11fec164d64682e3d7
Limezy <arnaudetpia@de-ruffray.fr>
2021-10-22 00:33:30 +0700

    Create unbound2md.py

---
+++ Script/unbound2md.py
@@ -0,0 +1,24 @@
+import re
+import fileinput
+import os
+import csv
+
+class Bible:
+	def __init__(name,abbrev):
+		self.name = name
+		self.abbrev = abbrev
+
+class BibleBook:
+	def __init__(name,abbrev,numberChapters):
+		self.name = name
+		self.abbrev = abbrev
+		self.numberChapters = numberChapters
+
+class BibleChapter:
+	def __init__(number):
+		self.number
+
+class BibleVerse:
+	def __init__():
+
+
